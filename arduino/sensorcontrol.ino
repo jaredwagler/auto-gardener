5ae8a3be292a47fb28e02512b54661a7e2c9fb27
Jared Wagler <jared.wagler@gmail.com>
2021-04-01 09:10:06 -0500

    Added DHT dependency and changed sensor pins

--- arduino/sensorcontrol.ino
+++ arduino/sensorcontrol.ino
@@ -3,32 +3,35 @@
 // Purpose:		Establish a serial connection with the Raspberry Pi and send sensor values to it
 // Author(s):		Jared Wagler, Sean McGuffey, Mife Atewogbola, Mouhammed Diagne
 
+#include "DHT.h"
+#define DHTPIN 3
+#define DHTTYPE DHT22
+
 //Definitions
 int humidPin = A0;
 int tempPin = A1;
-int moistPin = A2;
-
-//Create array to hold sensor values
-//Array location [0] is Humidity
-//Array location [1] is Temperature
-//Array location [2] is Moisture
-int sensorValues [3];
 
 //Establish Serial Connection
 void setup() {
+	//Create array to hold sensor values
+	//Array location [0] is Humidity
+	//Array location [1] is Temperature
+	//Array location [2] is Moisture
+	int sensorValues [3];
+	
 	//Determine pin layout of sensors
-	pinMode(humidPin, INPUT);
-	pinMode(tempPin, INPUT);
 	pinMode(moistPin, INPUT);
 	
 	//Start serial connection
 	Serial.begin(9600);
+	
+	dht.begin();
 }
 
 void loop() {
 	//Obtain current values from both sensors
-	sensorValues[0] = analogRead(humidPin,);
-	sensorValues[1] = analogRead(tempPin);
+	sensorValues[0] = dht.readHumidity();
+	sensorValues[1] = dht.readTemperature();
 	sensorValues[2] = analogRead(moistPin);
 	
 	//Do only if serial connection is online
