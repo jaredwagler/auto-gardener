f14b4cc484a51d38fb95a47822fde7e8f717fa3d
Mouhammed11 <mnd.86@me.com>
2021-04-01 09:59:58 -0500

    Update firebase.py

--- raspberrypi/firebase.py
+++ raspberrypi/firebase.py
@@ -1,10 +1,11 @@
-from firebase import Firebase
-
-config = {
-  "apiKey": "apiKey",
-  "authDomain": "projectId.firebaseapp.com",
-  "databaseURL": "https://databaseName.firebaseio.com",
-  "storageBucket": "projectId.appspot.com"
-}
-
-firebase = Firebase(config)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCVfS7WgMrIY7jDNA64zB1zqTtbqv2d6RM",
+  authDomain: "smartplant-c0ab0.firebaseapp.com",
+  databaseURL: "https://smartplant-c0ab0-default-rtdb.firebaseio.com",
+  projectId: "smartplant-c0ab0",
+  storageBucket: "smartplant-c0ab0.appspot.com",
+  messagingSenderId: "1008558540010",
+  appId: "1:1008558540010:web:ffa201571ec5f719528393",
+  measurementId: "G-PDV6GD4Z1W"
+};
